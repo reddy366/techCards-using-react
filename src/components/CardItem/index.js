a1e1fd79d921aa766349209b420265a823dbb5de
purushotham <spreddy366@gmail.com>
2022-03-19 07:38:08 +0000

    techcards solution

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const CardItem = props => {
+  const {cardData} = props
+  const {title, description, imgUrl, className} = cardData
+  return (
+    <li className={`${className} card-container`}>
+      <h1 className="card-title">{title}</h1>
+      <p className="card-description">{description}</p>
+      <div className="img-container">
+        <img className="card-img" src={imgUrl} alt={title} />
+      </div>
+    </li>
+  )
+}
+
+export default CardItem
